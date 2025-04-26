7b30c3cc45dd63bb4f902000dc89c3fd2097a7c1
Henrik Sandström <svartvikare@gmail.com>
2025-04-26 21:06:33 +0200

    Add router and health endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import Health from './Health.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+      <Routes>
+        <Route path="/health" element={<Health />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode >
 )
